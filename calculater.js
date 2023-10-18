2a54b07b547db84a1de78ef3e2b2c3c9cc344bfd
Muhammed Wasif <135797380+wasif415@users.noreply.github.com>
2023-10-18 10:28:46 +0500

    Add files via upload

--- calculater.js
+++ calculater.js
@@ -1,22 +1,55 @@
-function cal(){
-    var opr1 = document.getElementById('first').value;
-    var opr2 = document.getElementById('second').value;
-    var ope = document.getElementById('ope').value;
-    if (ope == '+'){
-        var res = parseInt(opr1) + parseInt(opr2);
-    }
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+}
 
-    if (ope == '-'){
-        var res = parseInt(opr1) - parseInt(opr2);
-    }
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
 
-    if (ope == '*'){
-        var res = parseInt(opr1) * parseInt(opr2);
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
     }
+}
 
-    if (ope == '/'){
-        var res = parseInt(opr1) / parseInt(opr2);
+function calculateSqrt() {
+    const display = document.getElementById('display');
+    const value = parseFloat(display.value);
+    if (!isNaN(value)) {
+        display.value = Math.sqrt(value);
+    } else {
+        display.value = 'Error';
     }
-    document.getElementById('res').value = res;
+}
 
+function calculatePow() {
+    const display = document.getElementById('display');
+    const value = parseFloat(display.value);
+    if (!isNaN(value)) {
+        display.value = Math.pow(value, 2);
+    } else {
+        display.value = 'Error';
+    }
+}
+
+function calculateSin() {
+    const display = document.getElementById('display');
+    const value = parseFloat(display.value);
+    if (!isNaN(value)) {
+        display.value = Math.sin(value * (Math.PI / 180)); // Convert degrees to radians
+    } else {
+        display.value = 'Error';
+    }
+}
+
+function calculateCos() {
+    const display = document.getElementById('display');
+    const value = parseFloat(display.value);
+    if (!isNaN(value)) {
+        display.value = Math.cos(value * (Math.PI / 180)); // Convert degrees to radians
+    } else {
+        display.value = 'Error';
+    }
 }
